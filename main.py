4cb4811df242bc7ce45b92f1e02af4fefd67f769
aaxlss <axl9305@gmail.com>
2022-09-13 17:41:54 +0100

    [Update] Addin method with query parameters and validation the values

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from fastapi import Body
 app = FastAPI()
 
@@ -29,7 +29,25 @@ def create_person(person: Person = Body()):
 
 @app.get('/person/detail')
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50), 
-    age: str = Query(...)
+    name: Optional[str] = Query(
+        None, 
+        min_length=1, 
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters" 
+        ), 
+    age: str = Query(
+        ...,
+        titel="Person age",
+        description="this is the person's name. It's required"
+        )
     ):
     return {'name':name, 'age':age}
+
+#validations path parameters
+#if there are 2 methos with the same name in python, the last one will be the method that will execute
+@app.get('/person/details/{person_id}')
+def show_person(
+    person_id: int = Path(..., gt = 0)
+):
+    return {person_id: 'It exists!'}
