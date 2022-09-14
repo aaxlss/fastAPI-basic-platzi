34f780640c48aff7d73ab476f99b7f9d13058933
aaxlss <axl9305@gmail.com>
2022-09-14 17:01:59 +0100

    response_model

--- main.py
+++ main.py
@@ -37,7 +37,27 @@ class Person(BaseModel):
         )
     hair_color:  Optional[HairColor] = Field(default=None)
     is_married: Optional[bool] = Field(default=None)
+    password: str = Field(..., min_length=8)
 
+class PersonOut(BaseModel):
+    first_name: str = Field(
+        ..., 
+        min_length=1,
+        max_length=50
+        )
+    last_name: str= Field(
+        ..., 
+        min_length=1,
+        max_length=50
+        )
+    age: int= Field(
+        ..., 
+        gt=0,
+        le=115
+
+        )
+    hair_color:  Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 #decorador para nombrar una funcion para un path
 @app.get('/')
 def home():
@@ -46,7 +66,7 @@ def home():
 
 
 #Request and response body
-@app.post('/person/new')
+@app.post('/person/new', response_model=PersonOut)
 def create_person(person: Person = Body()):
     return person
 
