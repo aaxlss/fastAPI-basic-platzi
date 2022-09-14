718c8d1768805301ab17b4816c98e035e29241fd
aaxlss <axl9305@gmail.com>
2022-09-14 17:06:13 +0100

    [Update] Removing duplicated code from Person and PersonOut creating a new Base which they will inherite from it. This has been made in this way to reduce code

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Location(BaseModel):
     state: str
     country: str
 
-class Person(BaseModel):
+class PersonBase(BaseModel):
     first_name: str = Field(
         ..., 
         min_length=1,
@@ -37,27 +37,14 @@ class Person(BaseModel):
         )
     hair_color:  Optional[HairColor] = Field(default=None)
     is_married: Optional[bool] = Field(default=None)
+    
+
+class Person(PersonBase):
     password: str = Field(..., min_length=8)
 
-class PersonOut(BaseModel):
-    first_name: str = Field(
-        ..., 
-        min_length=1,
-        max_length=50
-        )
-    last_name: str= Field(
-        ..., 
-        min_length=1,
-        max_length=50
-        )
-    age: int= Field(
-        ..., 
-        gt=0,
-        le=115
-
-        )
-    hair_color:  Optional[HairColor] = Field(default=None)
-    is_married: Optional[bool] = Field(default=None)
+class PersonOut(PersonBase):
+    pass
+    
 #decorador para nombrar una funcion para un path
 @app.get('/')
 def home():
